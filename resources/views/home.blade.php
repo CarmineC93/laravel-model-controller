ffcaf078e3666b7218730103b1f2a8af1739de3a
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-16 18:41:37 +0100

    print on page data of database  movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    <section class="d-flex">
+        <h2>Movies</h2>
+        @foreach ($movies as $movie)
+            <div class="card" style="width: 18rem;">
+                <img src="{{ $movie->image }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">{{ $movie->title }}</p>
+
+                </div>
+            </div>
+        @endforeach
+    </section>
 @endsection
