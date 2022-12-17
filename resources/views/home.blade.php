fa4eb6998939a2dbe44373da9f248435bc9d35a0
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-17 12:34:19 +0100

    add style to movies section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="d-flex">
-        <h2>Movies</h2>
-        @foreach ($movies as $movie)
-            <div class="card" style="width: 18rem;">
-                <img src="{{ $movie->image }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">{{ $movie->title }}</p>
+    <section>
+        <div class="container p-3 m-3">
+            <h2>Movies</h2>
+            <div class="row row-cols-5">
+                @foreach ($movies as $movie)
+                    <div class="col g-3">
+                        <div class="card">
+                            <img src="{{ $movie->image }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie->title }}</h5>
+                                <p class="card-text">{{ $movie->original_title }}</p>
+                                <p class="card-text">{{ $movie->date }}</p>
+                                <p class="card-text">{{ $movie->vote }}/10</p>
 
-                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </div>
     </section>
 @endsection
