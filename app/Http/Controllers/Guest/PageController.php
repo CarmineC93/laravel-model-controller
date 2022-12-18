bf34d568759d632d77081b7cbef03886c96f5c3f
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-18 14:25:38 +0100

    add some comments to PageController

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -8,17 +8,22 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+    //metodo che porta tutti i dati del database in pagina home
     public function index()
     {
         $movies = Movie::all();
         return view('home', compact('movies'));
     }
 
-    public function movies()  //probabilmente non necessaria
+    //metodo per avere tutti i dati del db su altra route/pagina con tutti i movies 
+    public function movies()
     {
         $movies = Movie::all();
         return view('movies.index', compact('movies'));
     }
+
+    /*metodo per avere dati del singolo film in una nuova route/pagina, che esegua un controllo nel caso
+    di inserimento indice da barra indirizzi. Se l'indice non esiste darà un errore 404 */
 
     public function show($movieIndex)
     {
