02717184a64ae26c08c633eccbffd1bbf1bdfb28
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-18 14:19:19 +0100

    single movie page works

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -2,21 +2,15 @@
 
 @section('content')
     <section>
-        <div class="container mt-3">
-            <h2>Movies</h2>
-            <div class="row row-cols-5">
-                @foreach ($movies as $index => $movie)
-                    <div class="col g-3">
-                        <div class="card">
-                            <img src="{{ $movie->image }}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $movie->title }}</h5>
-                                <p class="card-text">{{ $movie->original_title }}</p>
-                                <p class="card-text">{{ $movie->vote }}/10</p>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
+        <div class="container">
+            <div class="single-movie d-flex">
+                <img src="{{ $singleMovie['image'] }}" alt="{{ $singleMovie['title'] }}">
+                <div class="infos ms-4">
+                    <h2 class="text-light">Title: {{ $singleMovie['title'] }}</h2>
+                    <h4 class="text-light">Date: {{ $singleMovie['date'] }}</h4>
+                    <h4 class="text-light">Nationality: {{ $singleMovie['nationality'] }}</h4>
+                    <h4 class="text-light">Votations: {{ $singleMovie['vote'] }}/10</h4>
+                </div>
             </div>
         </div>
     </section>
